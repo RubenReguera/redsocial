3f608c1ba1b86bfc31f17fc00d533482aecdf4c6
Andrés <34494904+and-y93@users.noreply.github.com>
2018-05-23 19:03:44 +0200

    Add files via upload

---
+++ login_view.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <title>Feisbuk | Login</title>
+  </head>
+  <body>
+    
+    <div class="grid_login">
+    	<div class="grid_title d-flex justify-content-center align-items-center">
+    		<h1>Feisbuk</h1>
+    	</div>
+	    <form class="form-signin">
+	      <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+	      <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+	      <label for="user_login" class="sr-only">User</label>
+	      <input type="text" id="user_login" class="form-control rounded-0" placeholder="Nick" required autofocus>
+	      <label for="pass_login" class="sr-only">Password</label>
+	      <input type="password" id="pass_login" class="form-control rounded-0 mt-3" placeholder="Password" required>
+	      <button class="btn btn-lg btn-primary btn-block rounded-0" type="submit">Iniciar sesión</button>
+	      <p class="text-center">¿No tienes una cuenta? <br> Regístrate <a href="#">aquí</a></p>
+	    </form>
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
+</html>
