297f08f08a108fe44e1accef735eed0987aa93a3
constantynP <constantinpatuleastan@gmail.com>
2018-05-25 15:52:37 +0200

    db modificada

--- elfeisbuc_controller.php
+++ elfeisbuc_controller.php
@@ -7,4 +7,14 @@ class elfeisbuc extends CI_Controller {
         	$this->load->view('footer_view');
         }
 
+        
+
+       
+
+
+//validamos si es único
+$this->form_validation->set_rules("email","Email","trim|required|valid_email|xss_clean|is_unique[user.email]");
+//si no lo es mostramos un mensaje con set_message
+$this->form_validation->set_message('is_unique', 'El %s ya está ocupado.');
+
 }
